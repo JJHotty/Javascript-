0a27aabdd408ba5b3bb254887dc4a70a4b21a1a5
JJHotty <joshuastarrenburg.anthony@gmail.com>
2023-11-25 22:05:19 +1300

    I made a little thing
    I tried to make a pacman scroller... The idea was to have the little pacman move as the page did and eat little red dots.
    The theory was to have the ball move 4x the speed of the pacman meaning they would collide and the redball would reset and start to scroll again.
    
    I couldn't figure out why it felt and scrolled janky. The ball reset and stayed in place for a good while before continuing it's scroll.

---
+++ Application.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {
+    var scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollPercentage = (scrollPosition / scrollHeight) * 98.5;
+
+    var box = document.querySelector('.box');
+    box.style.left = scrollPercentage + '%';
+
+    var redDot = document.querySelector('#redDot');
+    var redDotPercentage = (scrollPercentage * 5) % 98.5;
+    redDot.style.right = redDotPercentage + '%';
+
+    if (scrollPercentage + 2 >= (100 - redDotPercentage)) {
+        // If the box has collided with the red dot, move the red dot back to the right
+        redDot.style.right = '2%';
+    }
+}
---
+++ Javascript Code/Notes.js
@@ -0,0 +1,46 @@
+// List of some common JavaScript commands
+var jsCommands = [
+    "alert", // Displays an alert box with a message and an OK button
+    "console.log", // Outputs a message to the web console
+    "document.getElementById", // Returns the element that has the ID attribute with the specified value
+    "document.createElement", // Creates an Element Node with the specified name
+    "document.querySelector", // Returns the first element that matches a specified CSS selector(s) in the document
+    "window.open", // Opens a new browser window
+    "window.close", // Closes the current window
+    "window.location", // Returns a Location object, which contains information about the URL of the document and provides methods for changing that URL
+    "Array.push", // Adds new items to the end of an array, and returns the new length
+    "Array.pop", // Removes the last element of an array, and returns that element
+    "Array.shift", // Removes the first element of an array, and returns that element
+    "Array.unshift", // Adds new items to the beginning of an array, and returns the new length
+    "Array.splice", // Adds/Removes items to/from an array, and returns the removed item(s)
+    "Array.slice", // Selects a part of an array, and returns the new array
+    "Array.sort", // Sorts the items of an array
+    "Array.reverse", // Reverses the order of the items in an array
+    "Array.concat", // Joins two or more arrays, and returns a copy of the joined arrays
+    "Array.join", // Joins all elements of an array into a string
+    "Array.map", // Creates a new array with the result of calling a function for each array element
+    "Array.filter", // Creates a new array with every element in an array that pass a test
+    "Array.reduce", // Reduce the values of an array to a single value (going left-to-right)
+    "Array.every", // Checks if every element in an array pass a test
+    "Array.some", // Checks if any element in an array pass a test
+    "Array.find", // Returns the value of the first element in an array that pass a test
+    "Array.findIndex", // Returns the index of the first element in an array that pass a test
+    "Array.fill", // Fill the elements in an array with a static value
+    "Array.from", // Creates a new, shallow-copied array instance from an array-like or iterable object
+    "Array.isArray", // Determines whether the passed value is an Array
+    "Array.of", // Creates a new Array instance with a variable number of arguments, regardless of number or type of the arguments
+    "Array.toString", // Converts an array to a string, and returns the result
+    "Array.toLocaleString", // Converts an array to a string, using the current locale
+    "Array.lastIndexOf", // Search the array for an element, starting at the end, and returns its position
+    "Array.includes", // Determines whether an array includes a certain value among its entries, returning true or false as appropriate
+    "Array.indexOf", // Search the array for an element and returns its position
+    "Array.keys", // Returns a new Array Iterator object that contains the keys for each index in the array
+    "Array.values", // Returns a new Array Iterator object that contains the values for each index in the array
+    "Array.entries", // Returns a new Array Iterator object that contains the key/value pairs for each index in the array
+    "Array.copyWithin", // Shallow copies part of an array to another location in the same array and returns it, without modifying its size
+    "Array.findLastIndex", // Returns the index of the last element in the array that satisfies the provided testing function. Otherwise, it returns -1, indicating that no element passed the test.
+    "Array.at", // Takes an integer value and returns the element at that index, allowing for positive and negative integers. Negative integers count back from the last item in the array.
+    "Array.flatMap", // Returns a new array formed by applying a given callback function to each element of the array, and then flattening the result by one level. It is identical to a map followed by a flat of depth 1.
+    "Array.forEach", // Executes a provided function once for each array element.
+    "Array.reduceRight", // Apply a function against an accumulator and each value of the array (from right-to-left) as to reduce it to a single value.
+];
